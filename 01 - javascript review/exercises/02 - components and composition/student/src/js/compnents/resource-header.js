1cdf3256cf8fccb5c7f3ee7adb45dcf8c34a3018
Noella Giang <noellagracegiang@gmail.com>
2026-01-21 15:06:00 -0700

    Class progress jan 20th

---
+++ 01 - javascript review/exercises/02 - components and composition/student/src/js/compnents/resource-header.js
@@ -0,0 +1,5 @@
+<div class="row g-3">
+  <resource-filters class="col-12 col-lg-4"></resource-filters>
+  <resource-results class="col-12 col-lg-4"></resource-results>
+  <resource-details class="col-12 col-lg-4"></resource-details>
+</div>
