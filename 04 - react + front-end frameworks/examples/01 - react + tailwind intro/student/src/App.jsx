c7a39af2600cab6c221832d0af4d6ad189cafe8b
Noella Giang <noellagracegiang@gmail.com>
2026-03-09 21:23:44 -0600

    Lesson 07 - React scaffold and static components

---
+++ 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header';
+import './App.css'
+
+
+function App() {
+  return <Header tagline="Find the right resources, right away" />;
+}
+
+export default App;
+
+ 
+
